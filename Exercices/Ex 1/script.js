cde8160a3fcb8081b93f1b98fbc54cd773c74b9c
Clément DELORD <cdelord@dawan.fr>
2022-01-17 12:14:35 +0100

    ex 4

---
+++ Exercices/Ex 1/script.js
@@ -0,0 +1,7 @@
+let voiture1 = "Mercedes";
+let voiture2 = "Ferrari";
+let modele1 = 34;
+let modele2 = 12.6;
+let phrase = voiture1 + ' ' + modele1 + ', ' + voiture2 + ' ' + modele2;
+
+console.log(phrase);
---
+++ Exercices/Ex 2/script.js
@@ -0,0 +1,5 @@
+function quelAgeAuraisJe(debut, fin) {
+    console.log((fin - debut) + ' ans');
+}
+
+quelAgeAuraisJe(1993, 2021);
