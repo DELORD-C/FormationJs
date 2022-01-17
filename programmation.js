fb6e6d52b23fdd1ef2734e235b75c30580cc1a60
Clément DELORD <cdelord@dawan.fr>
2022-01-17 11:32:37 +0100

    start

---
+++ programmation.js
@@ -0,0 +1,92 @@
+let variable1 = "let est utile pour déclarer une variable qu'on ne va pas trop modifier";
+var variable2 = "var est utile pour déclarer une variable qu'on va beaucoup modifier";
+const constante1 = "Cette valeur ne peut être modifiée";
+
+variable1 = "Plus besoin de déclaration pour modifier une variable existante";
+
+let a = 30;
+let b = 40;
+let c = a + b;
+let d = "30";
+// Les différents opérateur de comparaison :
+// ==           est égale à
+// ===          est égale à (type et valeur)
+// !=           est différent de
+// !==          est différent de (type et valeur)
+// <            est inférieur à
+// >            est supérieur à
+// <=           est inférieur ou égale à
+// >=           est supérieur ou égale à
+
+//ARRAY
+let monTableau = ["pomme", "banane", "fraise"]
+
+// STRUCTURES DE CONTROLE
+
+
+//CONDITIONS
+//IF
+if (a === d) {
+    console.log("a et d ont la même valeur et le même type");
+}
+else if (a == d) {
+    console.log("a et d ont la même valeur");
+}
+else {
+    console.log("a et d n'ont pas la même valeur");
+}
+
+//SWITCH
+switch (a) { // Switch permet de tester plusieurs valeur d'une variable
+    case 30:
+        console.log("a est égale à 30");
+        break;
+
+    case 40:
+        console.log("a est égale à 40");
+        break;
+
+    default:
+        console.log("la valeur de a est inconnue");
+        break;
+}
+
+//BOUCLES (Iterations)
+//FOR
+for (let i = 0; i < 10; i++) {
+    //console.log(i);
+}
+
+//FOREACH
+monTableau.forEach(fruit => {
+    console.log(fruit);
+});
+
+
+
+//FONCTIONS
+
+function maFonction (phrase) {
+    console.log(phrase);
+}
+
+maFonction("Cette phrase va être affichée par ma fonction");
+
+function compter (fin, départ = 0) { //définir une valeur par défaut d'un paramètre dans la déclaration d'une fonction permet de le rendre facultatif
+    for (let i = départ; i <= fin; i++) {
+        console.log(i);
+    }
+}
+
+compter(10);
+compter(10, 9);
+
+//MANIPULATION DE VARIABLES
+//CONCATENATION
+let debut = "Ceci est ";
+let fin = "une phrase complète.";
+let chiffre1 = 10;
+let chiffre2 = 10;
+let phrase = debut + fin + (chiffre1 + chiffre2);
+
+console.log(phrase);
