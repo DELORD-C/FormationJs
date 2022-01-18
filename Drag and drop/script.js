ab69702383aa2ebe427d72f1d698522261fb0466
Clément DELORD <cdelord@dawan.fr>
2022-01-18 15:18:43 +0100

    ex drag and drop corrigé

---
+++ Drag and drop/script.js
@@ -0,0 +1,41 @@
+let items = document.querySelectorAll('li'),
+    list = document.getElementById('list'),   
+    dragged,
+    draggedOn;
+
+items.forEach(function(item) {
+    item.addEventListener('dragstart', dragStart);
+    item.addEventListener('dragover', dragOver);
+    item.addEventListener('dragend', dragEnd);
+});
+
+function dragStart (e) {
+    dragged = e.target;
+    dragged.style.display = 'none';
+}
+
+function dragOver (e) {
+    if (e.target.tagName == 'LI') {
+        draggedOn = e.target;
+    }
+    else {
+        draggedOn = null;
+    }
+    items.forEach(function(item){
+        item.style.borderTop = 'none';
+    })
+    e.target.style.borderTop = 'solid 10px #a8ebff';
+}
+
+function dragEnd (e) {
+    if (draggedOn != null) {
+        dragged.remove();
+        list.insertBefore(dragged, draggedOn);
+    }
+    dragged.style.display = 'list-item';
+    dragged = null;
+    draggedOn = null;
+    items.forEach(function(item){
+        item.style.borderTop = 'none';
+    })
+}
