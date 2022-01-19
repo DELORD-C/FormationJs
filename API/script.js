618b70038424089e98f5fbd6af13ad79ab23286e
Clément DELORD <cdelord@dawan.fr>
2022-01-19 10:02:01 +0100

    aller plus loin

--- API/script.js
+++ API/script.js
@@ -25,3 +25,8 @@ function displayData (data) {
 // - Notre element UL se vide (on supprime tout son contenu)
 // - On refait une requête à l'api avec en paramètre la chaine de caractère contenue dans notre champs "search"
 // - On réaffiche nos résultats dans notre élément ul
+
+//Aller plus loin :
+// Ajouter un gif de chargement pendant la mise à jour des données
+// Supprimer le bouton OK et mettre en place un eventListenenr pour que la mise à jour se fasse automatiquement au changement de valeur de notre champ search
+// Ajouter un délai avant la mise à jour pour éviter les baisses de performances lors de la frappe
