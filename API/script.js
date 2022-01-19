331e78cd9e2c3057f18125e36a643c51375e1c0e
Clément DELORD <cdelord@dawan.fr>
2022-01-19 09:58:41 +0100

    énoncé api

--- API/script.js
+++ API/script.js
@@ -20,3 +20,8 @@ function displayData (data) {
         ul.append(li);
     }
 }
+
+//Faire en sorte que lorsque l'on appuie sur le bouton OK :
+// - Notre element UL se vide (on supprime tout son contenu)
+// - On refait une requête à l'api avec en paramètre la chaine de caractère contenue dans notre champs "search"
+// - On réaffiche nos résultats dans notre élément ul
