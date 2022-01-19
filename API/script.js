9bb0e73235e67136a4a8e217a008ed612defc236
Clément DELORD <cdelord@dawan.fr>
2022-01-19 10:47:24 +0100

    push

--- API/script.js
+++ API/script.js
@@ -1,17 +1,23 @@
 let ul = document.getElementById('list'),
     search = document.getElementById('search'),
+    btn = document.getElementById('btn'),
     request = new XMLHttpRequest();
 
-(function getResult() {
+function getResult(val = null) {
     request.onreadystatechange = function () {
         if (request.readyState === 4) {
             let data = JSON.parse(request.responseText);
             displayData(data.entries);
         }
     };
-    request.open('GET', 'https://api.publicapis.org/entries');
+    if (val == null) {
+        request.open('GET', 'https://api.publicapis.org/entries');
+    }
+    else {
+        request.open('GET', 'https://api.publicapis.org/entries?title=' + val);
+    }
     request.send();
-})()
+}
 
 function displayData (data) {
     for (let entrie of data) {
@@ -21,6 +27,15 @@ function displayData (data) {
     }
 }
 
+btn.addEventListener('click', () => {
+    ul.innerHTML = '';
+    let val = search.value;
+    console.log(val);
+    getResult(val);
+});
+
+getResult();
+
 //Faire en sorte que lorsque l'on appuie sur le bouton OK :
 // - Notre element UL se vide (on supprime tout son contenu)
 // - On refait une requête à l'api avec en paramètre la chaine de caractère contenue dans notre champs "search"
@@ -29,4 +44,4 @@ function displayData (data) {
 //Aller plus loin :
 // Ajouter un gif de chargement pendant la mise à jour des données
 // Supprimer le bouton OK et mettre en place un eventListenenr pour que la mise à jour se fasse automatiquement au changement de valeur de notre champ search
-// Ajouter un délai avant la mise à jour pour éviter les baisses de performances lors de la frappe
+// Ajouter un délai avant la mise à jour pour éviter les baisses de performances lors de la frappe$
