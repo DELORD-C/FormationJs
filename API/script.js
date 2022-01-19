547cd9f1efff6394b028a0dbfcd9fc60e8223601
Clément DELORD <cdelord@dawan.fr>
2022-01-19 09:56:57 +0100

    api ex

--- API/script.js
+++ API/script.js
@@ -1,15 +1,19 @@
-let request = new XMLHttpRequest();
-request.onreadystatechange = function () {
-    if (request.readyState === 4) {
-        let data = JSON.parse(request.responseText);
-        displayData(data.entries);
-    }
-};
-request.open('GET', 'https://api.publicapis.org/entries');
-request.send();
+let ul = document.getElementById('list'),
+    search = document.getElementById('search'),
+    request = new XMLHttpRequest();
+
+(function getResult() {
+    request.onreadystatechange = function () {
+        if (request.readyState === 4) {
+            let data = JSON.parse(request.responseText);
+            displayData(data.entries);
+        }
+    };
+    request.open('GET', 'https://api.publicapis.org/entries');
+    request.send();
+})()
 
 function displayData (data) {
-    let ul = document.getElementById('list');
     for (let entrie of data) {
         let li = document.createElement('li');
         li.innerHTML = '<a href="' + entrie.Link + '">' + entrie.API + '</a>';
